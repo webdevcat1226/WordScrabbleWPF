9a471266f0a6eb1e6fc50983d489410a562f4793
leosatan-fairy <tikhonovich0220@gmail.com>
2020-07-09 23:30:30 +1000

    add another logic

---
+++ README.md
@@ -0,0 +1,24 @@
+# <p align="center">Scrabble
+<p align="center">Scrabble game in C#, using <a href="https://en.wikipedia.org/wiki/Windows_Presentation_Foundation">WPF</a>.
+
+## Description
+* Standard 100-pieces version of Scrabble
+* No "Challenge" option
+* No "The Fifty Point Bonus"
+
+## Preview
+<details><summary>Click here</summary>
+  <p align="center">
+    <img src=".preview/home.jpg" alt="home"/>
+    <img src=".preview/desktop.jpg" alt="desktop"  />
+    <img src=".preview/mobile-and-text.jpg" alt="mobile-and-text" />
+  </p>
+</details>
+
+## To build the project
+You need Visual Studio >=2017. 
+
+## If you like this, please
+* Star
+* Fork
+* Contribute
---
+++ Scrabble2018/View/BlankTileForm.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Scrabble2018.View
+{
+    public partial class BlankTileForm : Window
+    {
+        public BlankTileForm()
+        {
+            InitializeComponent();
+            this.Topmost = true;
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            for( char i = 'A' ; i <= 'Z' ; i++ )
+            {
+                List.Items.Add(i);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
